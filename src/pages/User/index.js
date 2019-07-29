ca375524eed6e34bc14ab78399e70b2040b51120
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-07-29 12:59:18 -0300

    listing stars

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
-import api from '../../services/api'
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -14,25 +25,49 @@ export default class User extends Component {
     navigation: PropTypes.shape({
       getParam: PropTypes.func,
     }).isRequired,
-  }
+  };
 
   state = {
-    start: []
-  }
+    stars: [],
+  };
 
   async componentDidMount() {
-    const {navigation} = this.props;
+    const { navigation } = this.props;
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred)`;
+    const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({stars: response.data})
+    this.setState({ stars: response.data });
   }
 
   render() {
-    const { starts } = this.state;
-    return (
+    const { navigation } = this.props;
+    const { stars } = this.state;
 
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
     );
   }
 }
