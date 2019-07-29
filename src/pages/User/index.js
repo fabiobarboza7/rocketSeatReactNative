cf0cf8a5c5889b9f999043894ec8109a4cb5afdf
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-07-29 12:01:07 -0300

    searching starts

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import api from '../../services/api'
 
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
-  return (
-    <View>
-      <Text>{navigation.getParam('user')}</Text>
-    </View>
-  );
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  }
+
+  state = {
+    start: []
+  }
+
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred)`;
+
+    this.setState({stars: response.data})
+  }
+
+  render() {
+    const { starts } = this.state;
+    return (
+
+    );
+  }
 }
